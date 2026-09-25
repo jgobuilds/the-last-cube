fd7ba617d3636238d5e8037d14675d403670f121
jgobuilds <107055454+jgobuilds@users.noreply.github.com>
2026-09-25 07:07:03 -0400

    Apply new builds on the next load instead of after every tab closes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790334154|10885958';
+const CACHE_VERSION = '1790334413|10768298';
 /** @type {string} */
 const CACHE_PREFIX = 'The Last Cube-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
